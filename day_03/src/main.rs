9699913a92acca5eb4339548d7b5dc7afefe79e8
Dylan Law <law.dylan.t@gmail.com>
2021-01-11 18:07:38 -0500

    Add Day 3 Part Two

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -3,21 +3,29 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 
 const TREE_STRING: &str = "#";
-const SLOPE_RUN: usize = 3;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	let filename = &args[1];
+	let slope_x = args[2].parse::<usize>().unwrap();
+	let slope_y = args[3].parse().unwrap();
 	let reader = BufReader::new(File::open(filename).unwrap());
 
 	let mut trees = 0;
 	let mut pos = 0;
+	let mut y = slope_y;
 	for l in reader.lines() {
-		let line = l.unwrap();
-		if &line[pos..pos + 1] == TREE_STRING {
-			trees += 1;
+		if y == slope_y {
+			let line = l.unwrap();
+			if &line[pos..pos + 1] == TREE_STRING {
+				trees += 1;
+			}
+			pos = (pos + slope_x) % line.len();
+			y = 1;
+		} else {
+			y += 1;
 		}
-		pos = (pos + SLOPE_RUN) % line.len();
+
 	}
 
 	println!("{}", trees);
